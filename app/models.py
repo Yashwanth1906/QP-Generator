3d4da08a292a99a8fe0a95d2ed9e71a6caeb6665
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2024-03-04 07:39:53 +0530

    Topic-wise

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class questions(models.Model):
     CO = models.CharField(max_length=20,default = "")
     marks = models.IntegerField(default = 0)
     topic = models.CharField(max_length=200,default="")
-
+    course_code = models.CharField(max_length=50,default="")
 class partA(models.Model):
     difficulty1 = models.IntegerField(default=1,null=True)
     difficulty2 = models.IntegerField(default=1,null=True)
@@ -28,3 +28,16 @@ class partC(models.Model):
 
 class Image(models.Model):
     imagelink = models.CharField(max_length=500)
+
+class Department(models.Model):
+    department_id = models.CharField(max_length=20,primary_key = True)
+    department_name = models.CharField(max_length=50)
+
+class Courses(models.Model):
+    course_code = models.CharField(max_length = 50,primary_key = True)
+    course_name = models.CharField(max_length = 200)
+    semester_no = models.IntegerField()
+    department_id = models.ForeignKey(Department,on_delete=models.CASCADE)
+class Semesters(models.Model):
+    semester_no = models.IntegerField()
+    department_id = models.ForeignKey(Department,on_delete = models.CASCADE)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import random
 from django.shortcuts import render,redirect
-from .models import questions,partA,partC,partB,Image
+from .models import questions,partA,partC,partB,Image,Department,Semesters,Courses
 from .forms import ImageForm,wordreaderform
 import csv
 import io
@@ -55,7 +55,7 @@ def showqns(r):
     partb_diff1 = partb[0]['difficulty1']
     partb_diff2 = partb[0]['difficulty2']
     partb_diff3 = partb[0]['difficulty3']
-    f = questions.objects.all()
+    f = questions.objects.filter(course_code = "es3121")
     qn = f.values('question','Type','difficulty','marks','topic')
     questions1 = []
     for i in qn:
@@ -113,97 +113,83 @@ def showqns(r):
         for j in x:
             if i in j:
                 print("BYES")
-    print(questionb_paper)
     z = questionb_paper
     partaqns= []
     for i in questiona_paper:
         partaqns.append(i[0])
     questionb_paper = []
     questiona_paper = []
+    ele = []
+    twl = []
+    thir = []
+    four = []
+    fifteen =[]
+    count = 0
+    print(z)
+    for i in z:
+        if count>=0 and count<2:
+            ele.append(i)
+        elif count>=2 and count<4:
+            twl.append(i)
+        elif count>=4 and count<6:
+            thir.append(i)
+        elif count>=6 and  count<8:
+            four.append(i)
+        elif count>=8 and count<10:
+            fifteen.append(i)
+        count+=1
+    print(ele)
+    print()
+    print()
+    print(twl)
+    print()
+    print(thir)
+    print()
+    print(four)
+    print()
+    print(fifteen)
     a11 = []
     b11 = []
-    a12  =[]
+    a12 = []
     b12 = []
-    a13  =[]
+    a13 = []
     b13 = []
-    a14  =[]
+    a14 = []
     b14 = []
-    a15  =[]
+    a15 = []
     b15 = []
-    no = 0
-    questioninner = []
-    for i in z:
-        questioninner.append(i)
-    for i in questioninner:
-        if len(i)==4:
-            if no == 0:
-                a11.append(i[0])
-                no+=1
-                continue
-            elif no == 1:
-                b11.append(i[0])
-                no+=1
-            elif no == 2:
-                a12.append(i[0])
-                no+=1
-            elif no == 3:
-                b12.append(i[0])
-                no+=1
-            elif no == 4:
-                a13.append(i[0])
-                no+=1
-            elif no == 5:
-                b13.append(i[0])
-                no+=1
-            elif no == 6:
-                a14.append(i[0])
-                no+=1
-            elif no == 7:
-                b14.append(i[0])
-                no+=1
-            elif no == 8:
-                a15.append(i[0])
-                no+=1
-            elif no == 9:
-                b15.append(i[0])
-                no+=1
-            else:
-                break
-        elif len(i)>4:
-            for j in i:
-                if no == 0:
-                    a11.append(j[0])
-                elif no == 1:
-                    b11.append(j[0])
-                elif no == 2:
-                    a12.append(j[0])
-                elif no == 3:
-                    b12.append(j[0])
-                elif no == 4:
-                    a13.append(j[0])
-                elif no == 5:
-                    b13.append(j[0])
-                elif no == 6:
-                    a14.append(j[0])
-                elif no == 7:
-                    b14.append(j[0])
-                elif no == 8:
-                    a15.append(j[0])
-                elif no == 9:
-                    b15.append(j[0])
-                else:
-                    break
-            no+=1
-    print("a11:",end="")
-    print(a11)
-    print("b11:",end="")
-    print(b11)
-    message = ""
-    counta = 1
-    return render(r,"show.html",{'parta':partaqns,'partb':z,'a':code,'b':set,'c':branch,'d':date,'e':year,'f':semester,'g':assessment,'11a':a11,'11b':b11,'12a':a12,'12b':b12,'13a':a13,'13b':b13,'14a':a14,'14b':b14,'15a':a15,'15b':a15})
+    if len(ele)!=0:
+        if len(ele) == 1:
+            a11 = ele[0]
+        if len(ele) == 2:
+            a11 = ele[0]
+            b11 = ele[1]
+    if len(twl)!=0:
+        if len(twl) == 1:
+            a12 = twl[0]
+        else:
+            a12 = twl[0]
+            b12 = twl[1]
+    if len(thir)!=0:
+        if len(thir) == 1:
+            a13 = thir[0]
+        else:
+            a13 = twl[0]
+            b13 = thir[1]
+    if len(four)!=0:
+        if len(four) == 2:
+            b14 = four[1]
+        a14 = four[0]
+    if len(fifteen)!=0:
+        if len(fifteen) == 2:
+            b15 = fifteen[1]
+        a15 = fifteen[0]
+    return render(r,"show.html",{'parta':partaqns,'partb':z,'a':code,'b':set,'c':branch,'d':date,'e':year,'f':semester,'g':assessment,'11a':a11,'11b':b11,'12a':a12,'12b':b12,'13a':a13,
+                                 '13b':b13,'14a':a14,'14b':b14,'15a':a15,'15b':b15,})
     
 def showupload(r):
     msg = ''
+    x = "es3121"
     if r.POST:
         csv_file = r.FILES['word_upload']
         if csv_file:
@@ -226,7 +212,8 @@ def showupload(r):
                         BT = i[5],
                         CO =i[6],
                         marks = i[7],
-                        topic = i[8])
+                        topic = i[8],
+                        course_code = x)
                 except Exception as e:
                     msg += f"An unexpected error occurred during the upload process:{e}"
             f = questions.objects.all()
@@ -332,7 +319,7 @@ def solveB(diff,num,questions):
                     sixteens+=1
         check = min(tens,sixs) + min(fours,twelves) + (eights//2) + sixteens
         ques_per_topic.append(check)
-
+    
     if sum(ques_per_topic) < num:
         num = sum(ques_per_topic)
     
@@ -347,7 +334,9 @@ def solveB(diff,num,questions):
 
             if question[2]==16: 
                 #print("{}.) {} (16x1=16)".format(count+1,question[0]))
-                questionb_paper.append(question)
+                temp1 = []
+                temp1.append(question[0])
+                questionb_paper.append(temp1)
                 topic.remove(question)
                 count += 1
                 temp[questions.index(topic)]=0
@@ -361,13 +350,15 @@ def solveB(diff,num,questions):
                             c+=1
                 if c > 1:
                     #print("{}.) {}(8x1=8)".format(count+1,question[0]))
-                    questionb_paper.append(question)
+                    temp1 = []
+                    temp1.append(question[0])
                     topic.remove(question)
                     while True:
                         another = random.choice(topic)
                         if (another[2]==8) and (another[1]==diff):
                             #print("{}.) {} (8x1=8)".format(count+1,another[0]))
-                            questionb_paper.append(another)
+                            temp1.append(another[0])
+                            questionb_paper.append(temp)
                             topic.remove(another)
                             #print()
                             count+=1
@@ -383,13 +374,15 @@ def solveB(diff,num,questions):
                             c+=1
                 if c > 0:
                     #print("{}.) {} (12x1=12)".format(count+1,question[0]))
-                    questionb_paper.append(question)
+                    temp1 = []
+                    temp1.append(question[0])
                     topic.remove(question)
                     while True:
                         another = random.choice(topic)
                         if (another[2]==4) and (another[1]==diff):
                             #print("{}.) {} (4x1=4)".format(count+1,another[0]))
-                            questionb_paper.append(another)
+                            temp1.append(another[0])
+                            questionb_paper.append(temp1)
                             topic.remove(another)
                             #print()
                             count+=1
@@ -405,13 +398,15 @@ def solveB(diff,num,questions):
                             c+=1
                 if c > 0:
                     #print("{}.) {} (10x1=10)".format(count+1,question[0]))
-                    questionb_paper.append(question)
+                    temp1 = []
+                    temp1.append(question[0])
                     topic.remove(question)
                     while True:
                         another = random.choice(topic)
                         if (another[2]==6) and (another[1]==diff):
                             #print("{}.) {} (6x1=6)".format(count+1,another[0]))
-                            questionb_paper.append(another)
+                            temp1.append(another[0])
+                            questionb_paper.append(temp1)
                             topic.remove(another)
                             #print()
                             count+=1
